508cbfaf4a75075e263d620d26d8fd3600c18c52
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2025-02-20 21:30:30 -0800

    Update script.js

--- m07/js/script.js
+++ m07/js/script.js
@@ -1,28 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    function Hotel(name, rooms, booked) {
-        this.name = name;
-        this.rooms = rooms;
-        this.booked = booked;
-        this.checkAvailability = function () {
-            return this.rooms - this.booked;
-        };
+// Hotel Constructor Function
+function Hotel(name, rooms, booked) {
+    this.name = name;
+    this.rooms = rooms;
+    this.booked = booked;
+    this.checkAvailability = function() {
+        return this.rooms - this.booked;
+    };
+}
+
+// Hotel Objects
+var quayHotel = new Hotel("Quay", 40, 25);
+var parkHotel = new Hotel("Park", 120, 77);
+var sunsetHotel = new Hotel("Sunset", 86, 10);
+
+// Function to Display Hotel Info
+function displayHotelInfo(hotel, elementId) {
+    var hotelDiv = document.getElementById(elementId);
+    if (hotelDiv) {
+        hotelDiv.innerHTML = `
+            <h2>${hotel.name}</h2>
+            <p>Rooms: ${hotel.rooms}</p>
+            <p>Booked Rooms: ${hotel.booked}</p>
+            <p>Available Rooms: ${hotel.checkAvailability()}</p>
+        `;
     }
+}
 
-    // Create hotel objects
-    var quayHotel = new Hotel("Quay", 40, 25);
-    var parkHotel = new Hotel("Park", 120, 77);
-    var sunsetHotel = new Hotel("Sunset", 86, 10);
-
-    // Display hotel data in the correct elements
-    function displayHotelInfo(hotel, elementId) {
-        var hotelElement = document.getElementById(elementId);
-        if (hotelElement) {
-            hotelElement.innerHTML = "Available Rooms: " + hotel.checkAvailability();
-        }
-    }
-
-    // Call the function for each hotel
-    displayHotelInfo(quayHotel, "quay-availability");
-    displayHotelInfo(parkHotel, "park-availability");
-    displayHotelInfo(sunsetHotel, "sunset-availability");
-});
+// Display all hotels
+displayHotelInfo(quayHotel, "quay");
+displayHotelInfo(parkHotel, "park");
+displayHotelInfo(sunsetHotel, "sunset");
