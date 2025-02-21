7bc991046dce2cf594af8e1dd1e0f48fee9c6f4d
luckyykat <KATHERINELUCIANO1@GMAIL.COM>
2025-02-20 21:19:19 -0800

    Update script.js

--- m07/js/script.js
+++ m07/js/script.js
@@ -1,30 +1,28 @@
-// Define hotel constructor function
-function Hotel(name, rooms, booked) {
-    this.name = name;
-    this.rooms = rooms;
-    this.booked = booked;
-    this.checkAvailability = function() {
-        return this.rooms - this.booked;
-    };
-}
+document.addEventListener("DOMContentLoaded", function () {
+    function Hotel(name, rooms, booked) {
+        this.name = name;
+        this.rooms = rooms;
+        this.booked = booked;
+        this.checkAvailability = function () {
+            return this.rooms - this.booked;
+        };
+    }
 
-// Existing hotel objects
-var quayHotel = new Hotel("Quay", 40, 25);
-var parkHotel = new Hotel("Park", 120, 77);
+    // Create hotel objects
+    var quayHotel = new Hotel("Quay", 40, 25);
+    var parkHotel = new Hotel("Park", 120, 77);
+    var sunsetHotel = new Hotel("Sunset", 86, 10);
 
-// 🔥 New hotel object as per assignment requirements
-var sunsetHotel = new Hotel("Sunset", 86, 10);
+    // Display hotel data in the correct elements
+    function displayHotelInfo(hotel, elementId) {
+        var hotelElement = document.getElementById(elementId);
+        if (hotelElement) {
+            hotelElement.innerHTML = "Available Rooms: " + hotel.checkAvailability();
+        }
+    }
 
-// Function to display hotel details
-function displayHotelInfo(hotel, elementId) {
-    var hotelInfo = "<h2>" + hotel.name + "</h2>";
-    hotelInfo += "<p>Rooms: " + hotel.rooms + "</p>";
-    hotelInfo += "<p>Booked Rooms: " + hotel.booked + "</p>";
-    hotelInfo += "<p>Available Rooms: " + hotel.checkAvailability() + "</p>";
-    document.getElementById(elementId).innerHTML = hotelInfo;
-}
-
-// Display all three hotels
-displayHotelInfo(quayHotel, "hotel1");
-displayHotelInfo(parkHotel, "hotel2");
-displayHotelInfo(sunsetHotel, "hotel3"); // Displays Sunset Hotel
+    // Call the function for each hotel
+    displayHotelInfo(quayHotel, "quay-availability");
+    displayHotelInfo(parkHotel, "park-availability");
+    displayHotelInfo(sunsetHotel, "sunset-availability");
+});
